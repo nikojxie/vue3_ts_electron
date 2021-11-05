aecd40b907efda5559510c4c073d6cf612817001
xiejunyi <xiejunyi@dobest.com>
2021-11-05 16:35:36 +0800

    init

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -1,11 +1,19 @@
 <template>
   <div>1231233</div>
+  <el-button type="primary">Primary</el-button>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
   name: "Login",
-};
+  setup() {
+    console.log("setup");
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+});
 </script>
 
 <style scoped></style>
